a5cfa6cbe0dbf03b15109c74d0f7787565eed822
ahnafy <yasin011@morris.umn.edu>
2019-06-09 18:45:59 -0500

    add todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import TodoList from './todo';
+
 class App extends Component {
   render() {
     return (
         <div className="App">
+            <TodoList/>
         </div>
     )
   }
